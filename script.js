fb3ef0d9004d2789b3988a51f1a5964e47c6cef5
Dmytro <dm.kalitventsev@gmail.com>
2023-05-26 14:52:12 +0300

    added city list rendering function with weather

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 'use strict'
 
 class Weather {
-	#api = '';
+	#url = 'https://api.openweathermap.org';
+	#apiId = 'aca1888765eac88dd6aba8b4a07a1ab8';
 	#json = null;
 
-	constructor() {}
+	constructor() { }
 
 	get wrap() {
 		return document.querySelector('.weather');
@@ -18,33 +19,54 @@ class Weather {
 		return this.wrap.querySelector('.weather-cities__list');
 	}
 
-	getWeather() {
-		let request;
+	get weatherIcons() {
+		return ['day.svg', 'night.svg', 'cloudy.svg', 'cloudy-day.svg', 'cloudy-night.svg', 'rainy.svg', 'rainy-sun.svg', 'thunder.svg', 'snowy.svg', 'snowy-sun.svg',];
+	}
 
-		if (window.XMLHttpRequest) {
-			request = new XMLHttpRequest();
-		} else {
-			request = new ActiveXObject('Microsoft.XMLHttp');
-		}
+	get cities() {
+		return [
+			{ name: 'Полтава', id: 696643, },
+			{ name: 'Київ', id: 703448, },
+			{ name: 'Дніпро', id: 709930, },
+			{ name: 'Кременчук', id: 704147, },
+		];
+	}
 
-		request.open('GET', this.#api);
+	getWeather(citiesId, callback) {
+		let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHttp');
+
+		request.open('GET', `${this.#url}/data/2.5/weather?id=${citiesId}&appid=${this.#apiId}`);
 		request.responseType = 'json';
 
 		request.addEventListener('readystatechange', () => {
 			if (request.readyState === 4 && request.status === 200) {
 				this.#json = request.response;
-				
-				this.renderBlock();
+
+				callback();
+			}
+
+			if (request.readyState < 4 && request.status >= 400) {
+				console.error('Помилка даних');
 			}
 		});
 
 		request.send();
-
-		console.dir(request);
 	}
 
-	renderBlock() {
+	renderCities() {
+		this.cities.forEach(city => {
+			this.getWeather(city.id, () => {
+				const cityItem = `<li class="weather-cities__item" data-id="${city.id}">
+										<div class="weather-cities__picture">
+											<img src="img/weather/night.svg" alt="pic">
+										</div>
+										<span class="weather-cities__degrees">+25&#176;</span>
+										<span class="weather-cities__name">${city.name}</span>
+									</li>`;
 
+				this.weatherCitiesList.insertAdjacentHTML('beforeEnd', cityItem);
+			});
+		});
 	}
 
 	getCurrentDate() {
@@ -56,8 +78,8 @@ class Weather {
 		const month = (date.getMonth());
 		const weekday = date.getDay();
 
-		const currentDate = `<span class="weather-date__month">${nameMonths[month]} ${day}</span>
-							<span class="weather-date__day">${daysWeek[weekday - 1]}</span>`;
+		const currentDate = `<span>${nameMonths[month]} ${day}</span>
+							<span>${daysWeek[weekday - 1]}</span>`;
 
 		this.dateWrap.insertAdjacentHTML('beforeEnd', currentDate);
 	}
@@ -68,10 +90,17 @@ class Weather {
 		}
 	}
 
+	hideCitiesList(e) {
+		if (!e.target.matches('.weather-cities__button img')) {
+			this.weatherCitiesList.classList.remove('active');
+		}
+	}
+
 	init() {
-		this.getWeather();
 		this.getCurrentDate();
+		// this.renderCities();
 		this.wrap.addEventListener('click', this.showCitiesList.bind(this));
+		document.addEventListener('click', this.hideCitiesList.bind(this));
 	}
 }
 
